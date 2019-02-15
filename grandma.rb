f726feeda7e1b7dabf9ce940865e2aa6e4208bbe
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-15 20:48:22 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,17 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+puts "Hey Sonny! It's your lovely Grandmother! How are yeah?" 
+response = nil 
+bye = 0 
+while bye < 3 
+  response = gets.chomp 
+  if response == "I LOVE YOU" 
+    puts "I LOVE YOU TOO PUMPKIN!" 
+  elsif response == response.upcase 
+    puts "NO! NOT SINCE " + 1938 + "!" 
+  else 
+    puts "HUH?! SPEAK UP, SONNY!" 
+  end 
+end 
